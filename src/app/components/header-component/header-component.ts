7093466a16a2ffcbe62d1f5c6436954bba2cbfe2
birayaltun <149802729+birayaltun@users.noreply.github.com>
2026-02-14 00:57:33 +0300

    14 Şubat 01:00

--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
   templateUrl: './header-component.html',
   styleUrl: './header-component.css',
 })
 export class HeaderComponent {
+
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
 
 }
